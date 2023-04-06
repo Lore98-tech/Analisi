f35b48636e39ad1f8e71c8626597dde56edcecf8
Lorenzo Vincenti <lorenzovincenti98@gmail.com>
2023-04-06 09:51:19 +0200

    Cleaner file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Analisi
+#README
+
+This code plot functions y = f(x, k), where x is real and k is a natural number.
+To run the code, define a function in Successioni_di_funzioni.py. If possible, k starts from 0 or 1. 
+Then you can choose where to stop k, the plot interval on x axis, and a color palette.
+Moreover, there is the possibility to plot the derivates of the functions. 
+The plot limit on y axis is defined as a constant in function.py and can be changed. 
+
+List of functions:
+y = np.sin(np.power(x, 2)/k)
--- Successioni_di_funzioni.py
+++ Successioni_di_funzioni.py
@@ -8,37 +8,28 @@ print("Inserire il numero di funzioni da visualizzare:")
 n = input()
 n = int(n)
 
-#range di valori
 print("Inserisci intervallo [a, b]:")
 a = float(input("a = "))
 b = float(input("b = "))
 
 print("Plot delle funzioni")
 cf = cl.tavola()
-print("Plot delle derivate")
-cd = cl.tavola()
+print("Plot delle derivate? 'si' o 'no'")
+der = input()
+if der == 'si':
+    cd = cl.tavola()
 
 for k in range(0, n + 1):
 
     def funzione(x):
-        #y = (1/(1-x)*x*x - x/(2 - x * x))
-        #y = np.sin(k *x)
-        #y = np.power(np.power(k*x, 2) + 1, 1/2) - np.power(np.power(k*x, 2) - 1, 1/2)
-        #y = x/(1 + k * x)
-        #y = x/(1 + np.power(k*x, 2))
-        #y = np.power(k*x, 2)/(1 + np.power(k*x, 2))
-        #y = np.power((np.power(x, 2) - x),k)
-        #y = k*k/x
-        #y = np.sin(np.power(x, 2)/k) + (1 - np.power(1 - np.power(x, 2), 1/2))/k #[-1; 1]
-        #y = np.sin(np.power(x, 2)/k)
-        #y = (k * np.power(x, 3) + np.power((k + 1), 2) * np.sin(x))/(1 + np.power(k, 2)) #[-1; 1]
-        y = np.power(k, 3)/(np.power(k, 1) * 2 * x) * np.sin(x) + x * np.power((k + 1), 2)/(1 + np.power(k, 2)) - np.log(k * np.abs(x))
+
+        y = np.sin(np.power(x, 2)/k)
+
         return y
 
-#funzione
     f = fct.Function(funzione)
     f.plot(a, b, f.get_values_y(a, b), color = cl.color(n, k, cf))
-    f.plot(a, b, f.get_der(a, b), color = cl.color(n, k, cd))
-    #print(f.asintoti(a, b))
-    #f.asintoti_plot(a, b)
+    if der == 'si':
+        f.plot(a, b, f.get_der(a, b), color = cl.color(n, k, cd))
+    plt.pause(0.1)
 plt.show()
